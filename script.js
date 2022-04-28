21c1ea1123a9d523f86a1369dccc58bae03023fa
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-28 17:48:15 -0300

    requisito 5 concluído

--- script.js
+++ script.js
@@ -5,32 +5,34 @@ let elementP = document.createElement('p');
 elementP.setAttribute('id', 'carta-gerada');
 section1.appendChild(elementP)
 
-button1.addEventListener('click', function () {
+function alertMessenger() {
+  let text = inputText.value;
+  let textSplit = inputText.value.split('');
+  // console.log(text);
+  if (text.length === 0 || text === ' ') {
+    elementP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+  let conta = 0;
+  for(let index = 0; index < textSplit.length; index += 1){
+    if(textSplit[index] !== ' ') {
+      conta += 1;
+    }
+  }
+  if(conta === 0) {
+    elementP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+  console.log(conta);
+}
+
+function spanChild() {
   elementP.innerHTML = '';
   let text = inputText.value.split(' ');
-  for(let word of text){
+  for (let word of text) {
     let span = document.createElement('span');
     span.innerHTML = word;
     elementP.appendChild(span);
   }
+}
+button1.addEventListener('click', spanChild)
+button1.addEventListener('click', alertMessenger)
 
-  // function gerarCor() {
-  //   let r = parseInt(Math.random() * 255);
-  //   let g = parseInt(Math.random() * 255);
-  //   let b = parseInt(Math.random() * 255);
-  //   return `rgb(${r} ${g} ${b})`
-  // }
-
-  // for(let index = 0; index < span.length; index += 1) {
-  //   span[index].style.backgroundColor = gerarCor();
-
-  // }
-
-  // function alertMessenger() {
-  //   if (text.length === 0 || text.length === ' ') {
-  //     elementP.innerHTML = 'Por favor, digite o conteúdo da carta.';
-  //   }
-  // }
-  // alertMessenger();
-
-})
