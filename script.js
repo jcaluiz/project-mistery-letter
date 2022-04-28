d36af9c8bbfc3db160ef464946dfdbbc4bd77006
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-28 16:19:16 -0300

    requisito 3 concluído

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
 let inputText = document.querySelector('#carta-texto');
 let section1 = document.querySelector('#text');
 let button1 = document.querySelector('#criar-carta');
-let space = ' ';
 let elementP = document.createElement('p');
 elementP.setAttribute('id', 'carta-gerada');
 section1.appendChild(elementP)
 
-inputText.addEventListener('keyup', function () {
-  elementP.innerText = inputText.value;
-})
-
 button1.addEventListener('click', function () {
-  let text = elementP.innerText;
-  let splitText = text.split(' ');
-
-  for (let index = 0; index < splitText.length; index += 1) {
-    if(splitText) {
-      elementP.appendChild(document.createElement('span'));
-    }
+  elementP.innerHTML = '';
+  let text = inputText.value.split(' ');
+  for(let word of text){
+    let span = document.createElement('span');
+    span.innerHTML = word;
+    elementP.appendChild(span);
   }
-  let spanElement = document.getElementsByTagName('span');
 
-  for (let index = 0; index < splitText.length; index += 1) {
-    spanElement[index].classList.add('span-element');
-
-    // console.log(splitText[index]);
-  }
-  // let spanClasses = document.querySelectorAll('.span-element');
-  // let array = [];
-  // for (let index = 0; index < splitText.length; index += 1) {
-  //   array.push(splitText[index])
+  // function gerarCor() {
+  //   let r = parseInt(Math.random() * 255);
+  //   let g = parseInt(Math.random() * 255);
+  //   let b = parseInt(Math.random() * 255);
+  //   return `rgb(${r} ${g} ${b})`
   // }
-  // for (let index = 0; index < splitText.length; index += 1) {
-  //   array[index].appendChild(spanClasses[index])
+
+  // for(let index = 0; index < span.length; index += 1) {
+  //   span[index].style.backgroundColor = gerarCor();
+
   // }
-  // // console.log(splitText);
-  function alertMessenger() {
-    if (text.length === 0 || text.length === ' ') {
-      elementP.innerText = 'Por favor, digite o conteúdo da carta.';
-    }
-  }
-  alertMessenger();
+
+  // function alertMessenger() {
+  //   if (text.length === 0 || text.length === ' ') {
+  //     elementP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  //   }
+  // }
+  // alertMessenger();
 
 })
